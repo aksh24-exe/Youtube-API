e76e58445e107529761849f761a02fd9cc72c2a9
aksh24-exe <itsakshat07sharma@gmail.com>
2025-06-20 12:29:05 +0530

    Add Comment models

---
+++ models/comments.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    video_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+      require: true,
+    },
+    commentText: {
+      type: String,
+      require: true,
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+  },
+  { timestamps: true } // This will automatically add `createdAt` and `updatedAt` time
+);
